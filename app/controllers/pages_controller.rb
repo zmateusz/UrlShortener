7111b371de80f64eb64d0c73335cbfe0df2f1dc6
zmateusz <mat309@o2.pl>
2015-11-15 23:45:31 +0100

    add form and feature spec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
+    if params[:url] && !params[:url].empty?
+      @short = (0...6).map { (65 + rand(26)).chr }.join
+    end
   end
 end
---
+++ spec/features/pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe do
+  it 'displays welcome page' do
+    visit '/'
+    expect(page).to have_content 'Hello!'
+  end
+
+  it 'adds shortened url' do
+    visit '/'
+    fill_in('url', :with => 'https://elpassion.slack.com')
+    click_button 'Submit'
+    expect(page).to have_content 'https://elpassion.slack.com - short url added'
+  end
+
+  xit 'give back shorted url' do
+    visit '/short'
+    fill_in('url', with: 'https://elpassion.slack.com')
+    click_button 'Submit'
+    expect(page).to have_content 'elpassion'
+    click_link 'elpassion'
+    expect(current_url).to eq('https://elpassion.slack.com/')
+  end
+end
